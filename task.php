9b48f193a3d8efbe3d0aec14e9473429764fdd5c
Chapeau <37405123+ReportMyTeam@users.noreply.github.com>
2018-11-06 10:03:09 +0100

    Delete task.php

--- task.php
+++
@@ -1,9 +0,0 @@
-<?php ?>
-<?php
-$names = array("Patrick", "John", "Marie", "Claire", "Paul");
-foreach ($names as $name) {
-	if(strlen($name)>=2 && $name[0]=='p' && $name[0]=='a')
-   	print "$name, ";
-}
-?>
-
